f275247ce4245d7dee36f3238f272379106b0b48
Ezekude izuchukwu Francis <frankizuchukwu094@gmail.com>
2023-07-04 15:09:17 +0100

    Update settings.py
    edited

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # sending email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'frankizuchukwu094@gmail.com'
-EMAIL_HOST_PASSWORD = 'piufvkhfswhtlgmj'     
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''     
 # piufvkhfswhtlgmj
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
